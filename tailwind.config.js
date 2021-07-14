036e070e64c602b30a97e2fd6edb41f116e5e64d
Christian Pauly <christian-pauly@posteo.de>
2021-07-14 12:58:00 +0200

    Add tailwindcss custom color

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      purple: {
+        DEFAULT: '#5625BA',
+      },
+      white: {
+        DEFAULT: '#FFFFFF',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
