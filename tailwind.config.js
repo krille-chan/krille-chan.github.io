ff51063869cc74bd2f229ff3769ff8233b30af40
Krille Fear <christian-pauly@posteo.de>
2022-12-09 18:40:26 +0100

    chore: Background gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      purple: {
-        DEFAULT: '#5625BA',
-      },
-      white: {
-        DEFAULT: '#FFFFFF',
-      },
-    },
   },
   variants: {
     extend: {},
